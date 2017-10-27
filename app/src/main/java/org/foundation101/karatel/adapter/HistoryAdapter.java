bccb97a99872e3da0d6e495307822d80ec6ad839
dnk <fatnlazycat@gmail.com>
2017-10-27 11:14:30 +0300

    HistoryAdapter - added WebWiewClient callbacks for debugging

--- app/src/main/java/org/foundation101/karatel/adapter/HistoryAdapter.java
+++ app/src/main/java/org/foundation101/karatel/adapter/HistoryAdapter.java
@@ -3,15 +3,26 @@ package org.foundation101.karatel.adapter;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.AsyncTask;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.text.Html;
+import android.util.Log;
+import android.view.InputEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ClientCertRequest;
+import android.webkit.HttpAuthHandler;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.BaseAdapter;
@@ -148,6 +159,67 @@ public class HistoryAdapter extends BaseAdapter {
                     @Override
                     public void onPageFinished(WebView view, String url) {
                         //view.pageDown(true);
+                        Log.d("", "onPageFinished");
+                    }
+
+                    @Override
+                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                        super.onPageStarted(view, url, favicon);
+                    }
+
+                    @Override
+                    public void onLoadResource(WebView view, String url) {
+                        super.onLoadResource(view, url);
+                    }
+
+                    @Override
+                    public void onPageCommitVisible(WebView view, String url) {
+                        super.onPageCommitVisible(view, url);
+                    }
+
+                    @Override
+                    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                        super.onReceivedError(view, request, error);
+                    }
+
+                    @Override
+                    public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                        super.onReceivedHttpError(view, request, errorResponse);
+                    }
+
+                    @Override
+                    public void onFormResubmission(WebView view, Message dontResend, Message resend) {
+                        super.onFormResubmission(view, dontResend, resend);
+                    }
+
+                    @Override
+                    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                        super.onReceivedSslError(view, handler, error);
+                    }
+
+                    @Override
+                    public void onReceivedClientCertRequest(WebView view, ClientCertRequest request) {
+                        super.onReceivedClientCertRequest(view, request);
+                    }
+
+                    @Override
+                    public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
+                        super.onReceivedHttpAuthRequest(view, handler, host, realm);
+                    }
+
+                    @Override
+                    public void onUnhandledInputEvent(WebView view, InputEvent event) {
+                        super.onUnhandledInputEvent(view, event);
+                    }
+
+                    @Override
+                    public void onScaleChanged(WebView view, float oldScale, float newScale) {
+                        super.onScaleChanged(view, oldScale, newScale);
+                    }
+
+                    @Override
+                    public void onReceivedLoginRequest(WebView view, String realm, String account, String args) {
+                        super.onReceivedLoginRequest(view, realm, account, args);
                     }
                 });
                 int progress = holder.imageAnswer.getProgress();
