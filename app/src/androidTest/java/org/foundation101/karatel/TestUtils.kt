dd6754fe5c935e8200b1ff2ac3cd789b471ac6df
dnk <fatnlazycat@gmail.com>
2019-06-10 18:22:25 +0300

    scroll tests for NestedScrollView

--- app/src/androidTest/java/org/foundation101/karatel/TestUtils.kt
+++ app/src/androidTest/java/org/foundation101/karatel/TestUtils.kt
@@ -1,9 +1,16 @@
 package org.foundation101.karatel
 
+import android.view.View
+import androidx.core.widget.NestedScrollView
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anyOf
 
 object TestUtils {
     fun checkViewArrayIsDisplayed(views: Array<Int>) = views.forEach {
@@ -12,7 +19,9 @@ object TestUtils {
 
     fun checkViewArrayIsDisplayedWithScroll(views: Array<Int>) = views.forEach {
         Espresso.onView(ViewMatchers.withId(it))
-                .perform(ViewActions.scrollTo())
+                .perform(ScrollToAction())
+                //.perform(ViewActions.scrollTo())
+                //.perform(ViewActions.swipeUp())
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
@@ -21,4 +30,17 @@ object TestUtils {
                 .perform(ViewActions.scrollTo())
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
+}
+
+class ScrollToAction(
+    private val original: androidx.test.espresso.action.ScrollToAction = androidx.test.espresso.action.ScrollToAction()
+) : ViewAction by original {
+
+    override fun getConstraints(): Matcher<View> = anyOf(
+        allOf(
+            withEffectiveVisibility(Visibility.VISIBLE),
+            isDescendantOfA(isAssignableFrom(NestedScrollView::class.java))
+        ),
+        original.constraints
+    )
 }
